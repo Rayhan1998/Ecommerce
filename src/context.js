f945c19ad0b746b6ffaf0dad9ad444959d8e627f
Rayhan1998 <miahrayhan01@gmail.com>
2020-11-23 18:22:28 -0500

    added context to details page

--- src/context.js
+++ src/context.js
@@ -6,8 +6,26 @@ const ProductConext = React.createContext();
 // Consumer
 class ProductProvider extends Component {
   state = {
-    products: storeProducts,
+    products: [],
     detailProduct: detailProduct
+  };
+
+  componentDidMount() {
+    this.setProducts();
+  }
+
+  setProducts = () => {
+    let tempProducts = [];
+    storeProducts.forEach(item => {
+      const singleItem = { ...item };
+      tempProducts = [...tempProducts, singleItem];
+    });
+
+    this.setState(() => {
+      return {
+        products: tempProducts
+      };
+    });
   };
 
   handleDetail = () => {
