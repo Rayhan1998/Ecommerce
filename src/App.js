f6641fb4ce0e01c58b7593bc81819761fe59d09b
Rayhan1998 <miahrayhan01@gmail.com>
2020-11-29 15:18:48 -0500

    feat:setupCart

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from "./logo.svg";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import ProductList from "./components/ProductList";
 import Details from "./components/Details";
-import Cart from "./components/Cart";
+import Cart from "./components/Cart/Cart";
 import Default from "./components/Default";
+import Modal from "./components/Modal";
 
 function App() {
   return (
@@ -29,6 +29,7 @@ function App() {
           <Default />
         </Route>
       </Switch>
+      <Modal />
     </div>
   );
 }
--- src/context.js
+++ src/context.js
@@ -7,7 +7,13 @@ const ProductConext = React.createContext();
 class ProductProvider extends Component {
   state = {
     products: [],
-    detailProduct: detailProduct
+    detailProduct: detailProduct,
+    cart: [],
+    modalOpen: false,
+    modalProduct: detailProduct,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0
   };
 
   componentDidMount() {
@@ -28,21 +34,83 @@ class ProductProvider extends Component {
     });
   };
 
-  handleDetail = () => {
-    console.log("hello from detail");
+  getItem = id => {
+    const product = this.state.products.find(item => item.id === id);
+
+    return product;
+  };
+
+  handleDetail = id => {
+    const product = this.getItem(id);
+
+    this.setState(() => {
+      return { detailProduct: product };
+    });
   };
 
   addToCart = id => {
-    console.log(`id is ${id}`);
+    let tempProducts = [...this.state.products];
+    const index = tempProducts.indexOf(this.getItem(id));
+    const product = tempProducts[index];
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price;
+    product.total = price;
+
+    this.setState(
+      () => {
+        return {
+          products: tempProducts,
+          cart: [...this.state.cart, product]
+        };
+      },
+      () => {
+        console.log(this.state);
+      }
+    );
   };
 
+  openModal = id => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return {
+        modalProduct: product,
+        modalOpen: true
+      };
+    });
+  };
+  closeModal = () => {
+    this.setState(() => {
+      return { modalOpen: false };
+    });
+  };
+
+  increment = id => {
+    console.log("this is the increment");
+  };
+  decrement = id => {
+    console.log("this is the decrement");
+  };
+  removeItem = id => {
+    console.log("this is the remove");
+  };
+
+  clearCart = id => {
+    console.log("Cart was cleared");
+  };
   render() {
     return (
       <ProductConext.Provider
         value={{
           ...this.state,
           handleDetail: this.handleDetail,
-          addToCart: this.addToCart
+          addToCart: this.addToCart,
+          openModal: this.openModal,
+          closeModal: this.closeModal,
+          increment: this.increment,
+          decrement: this.decrement,
+          removeItem: this.removeItem,
+          clearCart: this.clearCart
         }}
       >
         {this.props.children}
